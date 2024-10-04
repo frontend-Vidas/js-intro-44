b4e0460ca3b2b07072a00650c057dcce6f172d03
tropikais <desertukas@yahoo.com>
2024-10-04 09:13:32 +0300

    For, 1-empty function

---
+++ uzd/1-empty.js
@@ -0,0 +1,9 @@
+console.clear();
+
+function tusciaFunkcija () {
+    return false;
+}
+console.log(tusciaFunkcija());
+console.log(tusciaFunkcija('asd')); // tikrinam
+console.log(tusciaFunkcija('true')); // tikrinam
+
